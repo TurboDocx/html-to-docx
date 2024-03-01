82b4e42cd14418dc6d54b7fc479bab4d5fd29b84
nicolasiscoding <nicolascoding@gmail.com>
2024-03-01 18:49:41 -0500

    chore: remove import mimeTypes from xml-builder

--- src/helpers/xml-builder.js
+++ src/helpers/xml-builder.js
@@ -10,7 +10,6 @@ import isVText from 'virtual-dom/vnode/is-vtext';
 import colorNames from 'color-name';
 import { cloneDeep } from 'lodash';
 import imageToBase64 from 'image-to-base64';
-import mimeTypes from 'mime-types';
 import sizeOf from 'image-size';
 import { getMimeType } from '../utils/image'
 
