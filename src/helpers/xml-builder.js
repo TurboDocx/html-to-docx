1da50af9d9855744431a2270830ddc13fe2e68f4
Kushal Kumar <59891164+K-Kumar-01@users.noreply.github.com>
2024-04-08 01:13:46 +0530

    Handle text-transform style attribute property (#11)
    * refactor: use loop to iterate over vnode style keys
    
    Signed-off-by: Kushal Kumar <kushalkumargupta4@gmail.com>
    
    * feat: handle text-transform style attribute
    
    Signed-off-by: K-Kumar-01 <59891164+K-Kumar-01@users.noreply.github.com>
    
    * docs: include text-transform testcase in the examples
    
    Signed-off-by: K-Kumar-01 <59891164+K-Kumar-01@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Kushal Kumar <kushalkumargupta4@gmail.com>
    Signed-off-by: K-Kumar-01 <59891164+K-Kumar-01@users.noreply.github.com>

--- src/helpers/xml-builder.js
+++ src/helpers/xml-builder.js
@@ -125,6 +125,19 @@ const fixupColorCode = (colorCodeString) => {
   }
 };
 
+const transformText = (text, transformation) => {
+  switch (transformation) {
+    case 'uppercase':
+      return text.toUpperCase();
+    case 'lowercase':
+      return text.toLowerCase();
+    case 'capitalize':
+      return text.replace(/\b\w/g, (char) => char.toUpperCase());
+    default:
+      return text;
+  }
+}
+
 const buildRunFontFragment = (fontName = defaultFont) =>
   fragment({ namespaceAlias: { w: namespaces.w } })
     .ele('@w', 'rFonts')
@@ -343,7 +356,7 @@ const fixupMargin = (marginString) => {
   } else if (inchRegex.test(marginString)) {
     const matchedParts = marginString.match(inchRegex);
     return inchToTWIP(matchedParts[1]);
-  } else if(percentageRegex.test(marginString)){
+  } else if (percentageRegex.test(marginString)) {
     // This requires changes in lot of functions. So, for now, we are returning the percentage value as it is.
     // TODO: Revisit this and see how margins in percentages are handled and change in respective functions.
     const matchedParts = marginString.match(percentageRegex);
@@ -427,138 +440,110 @@ const modifiedStyleAttributesBuilder = (docxDocumentInstance, vNode, attributes,
 
   // styles
   if (isVNode(vNode) && vNode.properties && vNode.properties.style) {
-    if (vNode.properties.style.color && !colorlessColors.includes(vNode.properties.style.color)) {
-      modifiedAttributes.color = fixupColorCode(vNode.properties.style.color);
-    }
+    const vNodeStyle = vNode.properties.style;
+    const vNodeStyleKeys = Object.keys(vNodeStyle);
 
-    if (
-      vNode.properties.style['background-color'] &&
-      !colorlessColors.includes(vNode.properties.style['background-color'])
-    ) {
-      modifiedAttributes.backgroundColor = fixupColorCode(
-        vNode.properties.style['background-color']
-      );
-    }
+    for (const vNodeStyleKey of vNodeStyleKeys) {
+      const vNodeStyleValue = vNodeStyle[vNodeStyleKey];
+      if (vNodeStyleKey === 'color') {
+        if (!colorlessColors.includes(vNodeStyleValue)) {
+          modifiedAttributes.color = fixupColorCode(vNodeStyleValue);
+        }
+      } else if (vNodeStyleKey === 'background-color') {
+        if (!colorlessColors.includes(vNodeStyleValue)) {
+          modifiedAttributes.backgroundColor = fixupColorCode(vNodeStyleValue);
+        }
+      } else if (vNodeStyleKey === 'background') {
+        if (!colorlessColors.includes(vNodeStyleValue)) {
+          modifiedAttributes.backgroundColor = fixupColorCode(vNodeStyleValue);
+        }
+      } else if (vNodeStyleKey === 'vertical-align') {
+        if (verticalAlignValues.includes(vNodeStyleValue)) {
+          modifiedAttributes.verticalAlign = vNodeStyleValue;
+        }
+      } else if (vNodeStyleKey === 'text-align') {
+        if (['left', 'right', 'center', 'justify'].includes(vNodeStyleValue)) {
+          modifiedAttributes.textAlign = vNodeStyleValue;
+        }
+      } else if (vNodeStyleKey === 'font-weight') {
+        // FIXME: remove bold check when other font weights are handled.
+        if (vNodeStyleValue === 'bold') {
+          modifiedAttributes.strong = vNodeStyleValue;
+        }
+      } else if (vNodeStyleKey === 'font-family') {
+        modifiedAttributes.font = docxDocumentInstance.createFont(vNodeStyleValue);
+      } else if (vNodeStyleKey === 'font-size') {
+        modifiedAttributes.fontSize = fixupFontSize(vNodeStyleValue, docxDocumentInstance);
+      } else if (vNodeStyleKey === 'line-height') {
+        modifiedAttributes.lineHeight = fixupLineHeight(vNodeStyleValue,
+          vNodeStyle['font-size'] ?
+            fixupFontSize(vNodeStyle['font-size'], docxDocumentInstance) :
+            null);
+      } else if (vNodeStyleKey === 'margin') {
+        const marginParts = vNodeStyleValue.split(' ');
+        const margins = {
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0
+        }
+        if (marginParts.length === 1) {
+          const fixedUpMargin = fixupMargin(marginParts[0]);
+          margins.top = fixedUpMargin;
+          margins.bottom = fixedUpMargin;
+          margins.left = fixedUpMargin;
+          margins.right = fixedUpMargin;
+        } else if (marginParts.length === 2) {
+          const fixedUpMarginVertical = fixupMargin(marginParts[0]);
+          const fixedUpMarginHorizontal = fixupMargin(marginParts[1]);
+          margins.top = fixedUpMarginVertical;
+          margins.bottom = fixedUpMarginVertical;
+          margins.left = fixedUpMarginHorizontal;
+          margins.right = fixedUpMarginHorizontal
+        } else if (marginParts.length === 3) {
+          margins.top = fixupMargin(marginParts[0]);
+          margins.bottom = fixupMargin(marginParts[2]);
+          margins.right = fixupMargin(marginParts[1]);
+          margins.left = fixupMargin(marginParts[1]);
+        } else if (marginParts.length === 4) {
+          margins.top = fixupMargin(marginParts[0]);
+          margins.right = fixupMargin(marginParts[1]);
+          margins.bottom = fixupMargin(marginParts[2]);
+          margins.left = fixupMargin(marginParts[3]);
+        }
 
-    if (
-      vNode.properties.style.background &&
-      !colorlessColors.includes(vNode.properties.style.background)
-    ) {
-      modifiedAttributes.backgroundColor = fixupColorCode(vNode.properties.style.background);
-    }
-
-    if (
-      vNode.properties.style['vertical-align'] &&
-      verticalAlignValues.includes(vNode.properties.style['vertical-align'])
-    ) {
-      modifiedAttributes.verticalAlign = vNode.properties.style['vertical-align'];
-    }
-
-    if (
-      vNode.properties.attributes.valign &&
-      verticalAlignValues.includes(vNode.properties.attributes.valign)
-    ) {
-      modifiedAttributes.verticalAlign = vNode.properties.attributes.valign;
-    }
-
-    if (
-      vNode.properties.style['text-align'] &&
-      ['left', 'right', 'center', 'justify'].includes(vNode.properties.style['text-align'])
-    ) {
-      modifiedAttributes.textAlign = vNode.properties.style['text-align'];
-    }
-
-    // FIXME: remove bold check when other font weights are handled.
-    if (vNode.properties.style['font-weight'] && vNode.properties.style['font-weight'] === 'bold') {
-      modifiedAttributes.strong = vNode.properties.style['font-weight'];
-    }
-    if (vNode.properties.style['font-family']) {
-      modifiedAttributes.font = docxDocumentInstance.createFont(
-        vNode.properties.style['font-family']
-      );
-    }
-    if (vNode.properties.style['font-size']) {
-      modifiedAttributes.fontSize = fixupFontSize(vNode.properties.style['font-size'], docxDocumentInstance);
-    }
-    if (vNode.properties.style['line-height']) {
-      modifiedAttributes.lineHeight = fixupLineHeight(
-        vNode.properties.style['line-height'],
-        vNode.properties.style['font-size']
-          ? fixupFontSize(vNode.properties.style['font-size'], docxDocumentInstance)
-          : null
-      );
-    }
-
-    if (vNode.properties.style['margin']) {
-      const marginParts = vNode.properties.style['margin'].split(' ');
-      const margins = {
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0
+        const { left, right, bottom } = margins
+        const indentation = { left, right }
+        if (left || right) {
+          modifiedAttributes.indentation = indentation;
+        }
+        if (bottom) {
+          modifiedAttributes.afterSpacing = bottom;
+        }
+      } else if (vNodeStyleKey === 'margin-left' || vNodeStyleKey === 'margin-right') {
+        const leftMargin = fixupMargin(vNodeStyle['margin-left']);
+        const rightMargin = fixupMargin(vNodeStyle['margin-right']);
+        const indentation = {};
+        if (leftMargin) {
+          indentation.left = leftMargin;
+        }
+        if (rightMargin) {
+          indentation.right = rightMargin;
+        }
+        if (leftMargin || rightMargin) {
+          modifiedAttributes.indentation = indentation;
+        }
+      } else if (vNodeStyleKey === 'margin-bottom') {
+        if (vNode.tagName === 'p') {
+          modifiedAttributes.afterSpacing = fixupMargin(vNodeStyle['margin-bottom']);
+        }
+      } else if (vNodeStyleKey === 'display') {
+        modifiedAttributes.display = vNodeStyle['display'];
+      } else if (vNodeStyleKey === 'width') {
+        modifiedAttributes.width = vNodeStyle['width'];
+      } else if (vNodeStyleKey === 'text-transform') {
+        modifiedAttributes.textTransform = vNodeStyleValue;
       }
-      if (marginParts.length === 1) {
-        const fixedUpMargin = fixupMargin(marginParts[0]);
-        margins.top = fixedUpMargin;
-        margins.bottom = fixedUpMargin;
-        margins.left = fixedUpMargin;
-        margins.right = fixedUpMargin;
-      } else if (marginParts.length === 2) {
-        const fixedUpMarginVertical = fixupMargin(marginParts[0]);
-        const fixedUpMarginHorizontal = fixupMargin(marginParts[1]);
-        margins.top = fixedUpMarginVertical;
-        margins.bottom = fixedUpMarginVertical;
-        margins.left = fixedUpMarginHorizontal;
-        margins.right = fixedUpMarginHorizontal
-      } else if (marginParts.length === 3) {
-        margins.top = fixupMargin(marginParts[0]);
-        margins.bottom = fixupMargin(marginParts[2]);
-        margins.right = fixupMargin(marginParts[1]);
-        margins.left = fixupMargin(marginParts[1]);
-      } else if (marginParts.length === 4) {
-        margins.top = fixupMargin(marginParts[0]);
-        margins.right = fixupMargin(marginParts[1]);
-        margins.bottom = fixupMargin(marginParts[2]);
-        margins.left = fixupMargin(marginParts[3]);
-      }
-
-      const { left, right, bottom } = margins
-      const indentation = { left, right }
-      if (left || right) {
-        modifiedAttributes.indentation = indentation;
-      }
-      if (bottom) {
-        modifiedAttributes.afterSpacing = bottom;
-      }
-    }
-
-    if (vNode.properties.style['margin-left'] || vNode.properties.style['margin-right']) {
-      const leftMargin = fixupMargin(vNode.properties.style['margin-left']);
-      const rightMargin = fixupMargin(vNode.properties.style['margin-right']);
-      const indentation = {};
-      if (leftMargin) {
-        indentation.left = leftMargin;
-      }
-      if (rightMargin) {
-        indentation.right = rightMargin;
-      }
-      if (leftMargin || rightMargin) {
-        modifiedAttributes.indentation = indentation;
-      }
-    }
-
-    // list elements might have margin-bottom style and happens in list where a p node exist for each list item
-    // copy the margin-bottom (if applicable) to the afterSpacing attribute
-    if (vNode.tagName === 'p' && vNode.properties.style['margin-bottom']) {
-      modifiedAttributes.afterSpacing = fixupMargin(vNode.properties.style['margin-bottom']);
-    }
-
-    if (vNode.properties.style.display) {
-      modifiedAttributes.display = vNode.properties.style.display;
-    }
-
-    if (vNode.properties.style.width) {
-      modifiedAttributes.width = vNode.properties.style.width;
     }
   }
 
@@ -677,15 +662,23 @@ const buildRun = async (vNode, attributes, docxDocumentInstance) => {
     ].includes(vNode.tagName)
   ) {
     const runFragmentsArray = [];
-
     let vNodes = [vNode];
     // create temp run fragments to split the paragraph into different runs
     let tempAttributes = cloneDeep(attributes);
+    // if any style tags are present use that to overwrite the attributes
     let tempRunFragment = fragment({ namespaceAlias: { w: namespaces.w } }).ele('@w', 'r');
+    let formattingFragmentAttributes = {}
     while (vNodes.length) {
       const tempVNode = vNodes.shift();
       if (isVText(tempVNode)) {
-        const textFragment = buildTextElement(tempVNode.text);
+        const textFragment = buildTextElement(transformText(tempVNode.text, formattingFragmentAttributes.textTransform || tempAttributes.textTransform));
+        // we don't need to pass the formattingFragmentAttributes to the buildRunProperties function
+        // since the attributes are already applied to the runPropertiesFragment
+        // if the children is text then we directly reach this if node
+        // if the node is a span, then those attributes are passed to the buildRunOrRuns function
+        // and the attributes are applied to the runPropertiesFragment
+        // if node is formatting node, then the attributes are stored in formattingFragmentAttributes
+        // and are applied to the text node as required
         const tempRunPropertiesFragment = buildRunProperties({ ...attributes, ...tempAttributes });
         tempRunFragment.import(tempRunPropertiesFragment);
         tempRunFragment.import(textFragment);
@@ -713,7 +706,6 @@ const buildRun = async (vNode, attributes, docxDocumentInstance) => {
             'pre',
           ].includes(tempVNode.tagName)
         ) {
-          tempAttributes = {};
           switch (tempVNode.tagName) {
             case 'strong':
             case 'b':
@@ -733,16 +725,19 @@ const buildRun = async (vNode, attributes, docxDocumentInstance) => {
               break;
           }
           const formattingFragment = buildFormatting(tempVNode);
-
+          formattingFragmentAttributes = modifiedStyleAttributesBuilder(docxDocumentInstance, tempVNode, { ...formattingFragmentAttributes });
           if (formattingFragment) {
             runPropertiesFragment.import(formattingFragment);
           }
           // go a layer deeper if there is a span somewhere in the children
         } else if (tempVNode.tagName === 'span') {
+
+          const modifiedAttributes = modifiedStyleAttributesBuilder(docxDocumentInstance, tempVNode, { ...attributes, ...tempAttributes });
+
           // eslint-disable-next-line no-use-before-define
           const spanFragment = await buildRunOrRuns(
             tempVNode,
-            { ...attributes, ...tempAttributes },
+            modifiedAttributes,
             docxDocumentInstance
           );
 
@@ -795,9 +790,8 @@ const buildRun = async (vNode, attributes, docxDocumentInstance) => {
 
       if (tempVNode.children && tempVNode.children.length) {
         if (tempVNode.children.length > 1) {
-          attributes = { ...attributes, ...tempAttributes };
+          attributes = modifiedStyleAttributesBuilder(docxDocumentInstance, tempVNode, { ...attributes, ...tempAttributes });
         }
-
         vNodes = tempVNode.children.slice().concat(vNodes);
       }
     }
@@ -808,7 +802,7 @@ const buildRun = async (vNode, attributes, docxDocumentInstance) => {
 
   runFragment.import(runPropertiesFragment);
   if (isVText(vNode)) {
-    const textFragment = buildTextElement(vNode.text);
+    const textFragment = buildTextElement(transformText(vNode.text, attributes.textTransform));
     runFragment.import(textFragment);
   } else if (attributes && attributes.type === 'picture') {
 
