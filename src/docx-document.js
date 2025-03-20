67e9d3adb6ea67005c4958d0807d3e7d5bfc12b0
nicolasiscoding <nicolascoding@gmail.com>
2025-03-20 10:51:07 -0400

    chore: remove extra comment

--- src/docx-document.js
+++ src/docx-document.js
@@ -163,13 +163,10 @@ class DocxDocument {
     this.pageNumber = properties.pageNumber || false;
     this.skipFirstHeaderFooter = properties.skipFirstHeaderFooter || false;
     this.lineNumber = properties.lineNumber ? properties.lineNumberOptions : null;
-
-    // Follow the same pattern as other table properties
     this.addSpacingAfterTable =
       properties.table && properties.table.addSpacingAfter !== undefined
         ? properties.table.addSpacingAfter
         : defaultDocumentOptions.table.addSpacingAfter;
-
     this.lastNumberingId = 0;
     this.lastMediaId = 0;
     this.lastHeaderId = 0;
